05ec30e676a903af6f69770ddd41a15a4bfc1d01
Jacmiz <158401640+Jacmiz@users.noreply.github.com>
2025-05-20 10:27:28 +1000

    membership additions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-script
+function writeTest(){
+// Requiring fs module in which
+// writeFile function is defined.
+const fs = require('fs')
+
+// Data which will write in a file.
+let data = "Hello world."
+
+// Write data in 'Hello.txt' .
+fs.writeFile('Hello.txt', data, (err) => {
+
+    // In case of a error throw err.
+    if (err) throw err;
+})
+}
